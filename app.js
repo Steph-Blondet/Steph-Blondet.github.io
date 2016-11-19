f93f9f5313cd40944413531f7f39fc1bb90c33b8
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2016-11-19 06:54:09 -0500

    created divs inside #board and gave it a class of .squares

--- app.js
+++ app.js
@@ -22,19 +22,49 @@ $(function() {
   // add an event listener with
 
 
+// var winningOptions = [
+//   [1, 2, 3, 4], [2, 3, 4, 5], [3, 4, 5, 6], [4, 5, 6, 7]
+// ];
+//
+// var wins = function() {
+//   console.log('checking winning conditions');
+//   var horizontalWins = document.querySelector('---');
+//
+//   for (var i = 0; i < winningOptions.length; i++) {
+//     winningOptions[i]
+//   }
+//
+//   if (winningOptions === true) {
+//
+//   }
+// }
+//
+// wins();
 
+// CREATING CONNECT FOUR BOARD
+var $board = $('#board'); // selecting board id to create board
 
+for (var i = 0; i < 42; i++) { // iterating 42 times
+  var $newBoardDiv = $('<div>'); // creating a new div that goes below the div board
+  $newBoardDiv.addClass('.squares'); // adding the class squares to the board div
+  $board.append($newBoardDiv); // appending the new div that goes inside the board div
+  $newBoardDiv.on('click', function(){
 
-
-
-
-
-
-
-
-
-
-
-
-
+  });
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
