54579cf8370f09cf293d4ea8bea387dd34f45cdd
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2016-11-18 22:13:00 -0500

    first attempt at pseudocode - random thoughts

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 console.log('testing connect four file');
+
+$(function() {
+
+// create a board with 42 squares?
+  // write a function that saves the squares in a variable
+  // write for loop that generates 42 squares and gives them a click event listener (iterates 42 times?)
+  // when the event listener is clicked create function
+
+// create a function that runs every time a square is clicked
+  // write if/else statement so clicked squares do not respond to additional clicks
+
+// write an array with all the possible winning conditions
+// write a function to check all the winning possibilities
+// select the squares
+// write a for loop that loops over the winning conditions array
+
+
+
+// create a button that resets the game every time that is clicked
+  // create a variable that selects the button
+  // add an event listener with
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
