3ea5ab5384f7d2983906fa039e3380566346e573
Stephanie Blondet <stephanieblondet@Stephanies-iMac.local>
2016-11-19 19:26:07 -0500

    tried some js...

---
+++ second_attempt/app.js
@@ -0,0 +1,54 @@
+console.log('testing connect four file');
+
+$(function() {
+
+// // SELECTING ELEMENTS
+//   var $board = $('#board'); // selecting board id to...
+//
+// // ADDING TILE NUMBERS TO EACH ID
+//   for (var i = 0; i < 42; i++) { // iterating 42 times
+//     var $circleClass = $('.circle'); // selecting the class circle
+//     $circleClass.attr('id', i); // every time the loop runs, add a tile attr with a number from 0-41
+//     $board.append($circleClass);
+//
+//   };
+
+
+
+
+// for (var i = 0; i < 42; i++) { // iterating 42 times
+//     var $newBoardDiv = $('<div>'); // creating a new div that goes below the div board
+//       $newBoardDiv.addClass('squares'); // adding the class squares to the board div
+//       $newBoardDiv.attr('tile', i);
+//       $board.append($newBoardDiv); // appending the new div that goes inside the board div
+//
+//     var $secondDiv = $('<div>'); // creating a new div that goes below the new board div
+//       $secondDiv.attr('id', 'circle'); // gave attribute of circle to the new div
+//       // $secondDiv.attr('tile', i); // every time the loop runs, add a tile attr with a number from 0-41
+//       $newBoardDiv.append($secondDiv); // appending the circle div that goes inside the $newBoardDiv
+//
+//       $newBoardDiv.on('click', $toggleFunction); // on click, the square will...
+//
+//
+//   };
+
+});
+
+
+
+
+
+/////////////////////////////////////////
+// *** THOUGHTS AND PRACTICE CODE *** //
+///////////////////////////////////////
+
+// // SELECTING ELEMENTS
+//   var $board = $('#board'); // selecting board id to...
+//
+// // ADDING TILE NUMBERS TO EACH ID
+//   for (var i = 0; i < 42; i++) { // iterating 42 times
+//     var $circleClass = $('.circle'); // selecting the class circle
+//     $circleClass.attr('tile', i); // every time the loop runs, add a tile attr with a number from 0-41
+//     $board.append($circleClass);
+//
+//   };
